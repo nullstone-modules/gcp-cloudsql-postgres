b912b5808e0ef109de92ee6ee17550a8988797c9
Brad Sickles <brad.sickles@gmail.com>
2022-06-11 16:48:40 -0400

    contract

--- nullstone.tf
+++ nullstone.tf
@@ -6,33 +6,20 @@ terraform {
   }
 }
 
+data "ns_workspace" "this" {}
+
+// Generate a random suffix to ensure uniqueness of resources
 resource "random_string" "resource_suffix" {
   length  = 5
   lower   = true
   upper   = false
-  number  = false
+  numeric = false
   special = false
 }
 
-data "ns_workspace" "this" {}
-
-data "ns_connection" "network" {
-  name = "network"
-  type = "network/gcp"
-}
-
 locals {
-  resource_name = "${data.ns_workspace.this.block_ref}-${random_string.resource_suffix.result}"
   tags          = data.ns_workspace.this.tags
+  block_name    = data.ns_workspace.this.block_name
+  resource_name = "${data.ns_workspace.this.block_ref}-${random_string.resource_suffix.result}"
   labels        = { for key, value in local.tags : lower(key) => value }
-  vpc_id        = data.ns_connection.network.outputs.vpc_id
-  vpc_name      = data.ns_connection.network.outputs.vpc_name
-}
-
-data "google_compute_network" "vpc" {
-  name = local.vpc_name
-}
-
-data "google_compute_subnetwork" "private0" {
-  self_link = data.ns_connection.network.outputs.private_subnet_self_links[0]
 }
