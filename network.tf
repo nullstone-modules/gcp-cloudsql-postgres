89701274b8b633e3b2c1f50dbcfd679de30ca947
Brad Sickles <brad.sickles@gmail.com>
2023-06-10 19:02:22 -0400

    Fixed network connection

--- network.tf
+++ network.tf
@@ -5,12 +5,13 @@ data "ns_connection" "network" {
 
 locals {
   vpc_id                    = data.ns_connection.network.outputs.vpc_id
+  vpc_name                  = data.ns_connection.network.outputs.vpc_name
   vpc_access_connector      = data.ns_connection.network.outputs.vpc_access_connector
-  private_subnet_self_links = data.ns_connection.network.outputs.private_subnet_self_links[0]
+  private_subnet_self_links = data.ns_connection.network.outputs.private_subnet_self_links
 }
 
 data "google_compute_network" "vpc" {
-  name = local.vpc_id
+  name = local.vpc_name
 }
 
 data "google_compute_subnetwork" "private0" {
