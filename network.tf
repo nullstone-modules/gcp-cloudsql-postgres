2fc88525adb9a336aa264ca22d9c0c10d40860c6
Brad Sickles <brad.sickles@gmail.com>
2023-06-10 11:01:25 -0400

    Align to mysql

--- network.tf
+++ network.tf
@@ -1,18 +1,18 @@
 data "ns_connection" "network" {
   name     = "network"
-  type     = "network/gcp"
   contract = "network/gcp/vpc"
 }
 
 locals {
-  vpc_id   = data.ns_connection.network.outputs.vpc_id
-  vpc_name = data.ns_connection.network.outputs.vpc_name
+  vpc_id                    = data.ns_connection.network.outputs.vpc_id
+  vpc_access_connector      = data.ns_connection.network.outputs.vpc_access_connector
+  private_subnet_self_links = data.ns_connection.network.outputs.private_subnet_self_links[0]
 }
 
 data "google_compute_network" "vpc" {
-  name = local.vpc_name
+  name = local.vpc_id
 }
 
 data "google_compute_subnetwork" "private0" {
-  self_link = data.ns_connection.network.outputs.private_subnet_self_links[0]
+  self_link = local.private_subnet_self_links[0]
 }
--- service_networking.tf
+++ service_networking.tf
@@ -1,17 +1,17 @@
+resource "google_compute_global_address" "db_private" {
+  name          = "${local.resource_name}-private-ip"
+  purpose       = "VPC_PEERING"
+  address_type  = "INTERNAL"
+  prefix_length = 16
+  network       = local.vpc_id
+}
+
 resource "google_project_iam_member" "service_agent" {
   depends_on = [google_project_service.service_networking]
 
   project = local.project_id
   member  = "serviceAccount:service-${local.project_number}@service-networking.iam.gserviceaccount.com"
   role    = "roles/servicenetworking.serviceAgent"
-}
-
-resource "google_compute_global_address" "db_private" {
-  name          = local.resource_name
-  purpose       = "VPC_PEERING"
-  address_type  = "INTERNAL"
-  prefix_length = 16
-  network       = local.vpc_name
 }
 
 resource "google_service_networking_connection" "private_vpc_connection" {
