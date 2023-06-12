4d9adfaad39e8c9cda6ddaa02389973f28ae9ec4
Brad Sickles <brad.sickles@gmail.com>
2023-06-12 10:26:21 -0400

    upgrade pg-db-admin

--- db-admin.tf
+++ db-admin.tf
@@ -1,6 +1,6 @@
 module "db_admin" {
   source  = "api.nullstone.io/nullstone/gcp-pg-db-admin/gcp"
-  version = "~> 0.1.0"
+  version = "~> 0.6.0"
 
   name   = local.resource_name
   labels = local.labels
